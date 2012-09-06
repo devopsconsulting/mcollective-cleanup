d937d054dbbbf0a4eb04f87c11909f2b27bfe036
Frank Spijkerman <frank.spijkerman@avira.com>
2012-09-06 19:16:11 +0000

    Meh changed prefix back + better cleanup

--- build-package-fedora.sh
+++ build-package-fedora.sh
@@ -2,12 +2,13 @@
 
 VERSION=`git shortlog | grep -E '^[ ]+\w+' | wc -l`
 
+rm *.rpm
 fpm -s dir -t rpm -a all  \
 	--name "mcollective-plugins-cleanup" \
 	--version "0.$VERSION" \
 	--maintainer "Frank Spijkerman <frank.spijkerman@avira.com>" \
 	--url "https://github.dtc.avira.com/VDT/mcollective-cleanup" \
 	--depends "mcollective" \
-	--prefix "/usr/share/mcollective/plugins/mcollective" \
-	--exclude "README.rst" \
+	--prefix "/usr/libexec/mcollective/mcollective" \
+	--exclude "README.rst" --exclude "build*.sh" \
     *
